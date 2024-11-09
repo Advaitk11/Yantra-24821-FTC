0c17624eac7bd4f730724794e5bf76ecfc6f2769
Triticism <kanishk.deshmukh10@gmail.com>
2024-11-09 09:29:50 -0800

    Add files via upload

---
+++ AutoTest.py
@@ -0,0 +1,53 @@
+from com.qualcomm.robotcore.eventloop.opmode import Autonomous
+from com.qualcomm.robotcore.eventloop.opmode import LinearOpMode
+from com.qualcomm.robotcore.hardware import DcMotor, Servo
+from com.qualcomm.robotcore.util import ElapsedTime
+
+@Autonomous(name="Autonomous code blue inside 23/24 PYTHON copy")
+class Autonomouscodeblueinside2324(LinearOpMode):
+
+    def runOpMode(self):
+        # Initialize hardware variables
+        self.leftslide = self.hardwareMap.get(Servo, "leftslide")
+        self.rightslide = self.hardwareMap.get(Servo, "rightslide")
+        self.frontleft = self.hardwareMap.get(DcMotor, "front left")
+        self.frontright = self.hardwareMap.get(DcMotor, "front right")
+        self.rearleft = self.hardwareMap.get(DcMotor, "rear left")
+        self.rearright = self.hardwareMap.get(DcMotor, "rear right")
+
+        # Initialize the timer
+        timer = ElapsedTime()
+
+        # Wait for the game to start (driver presses PLAY)
+        self.waitForStart()
+
+        # Autonomous actions
+        if self.opModeIsActive():
+            # Reset timer and move robot forward
+            timer.reset()
+            while self.opModeIsActive() and timer.time() <= 1:
+                self.frontleft.setPower(-1)
+                self.frontright.setPower(1)
+                self.rearleft.setPower(-1)
+                self.rearright.setPower(1)
+
+            # Continue moving in a different direction (Right)
+            while self.opModeIsActive() and timer.time() <= 2:
+                self.frontleft.setPower(-1)
+                self.frontright.setPower(-1)
+                self.rearleft.setPower(1)
+                self.rearright.setPower(1)
+
+            # Continue moving in a different direction (Left)
+            while self.opModeIsActive() and timer.time() <= 3:
+                self.frontleft.setPower(1)
+                self.frontright.setPower(-1)
+                self.rearleft.setPower(1)
+                self.rearright.setPower(-1)
+
+            # Move backwards to starting point
+            while self.opModeIsActive() and timer.time() <= 4:
+                self.frontleft.setPower(-1)
+                self.frontright.setPower(-1)
+                self.rearleft.setPower(1)
+                self.rearright.setPower(1)
