898c60e4748d5426776a33e658cf9b668471ef9d
Triticism <kanishk.deshmukh10@gmail.com>
2024-11-11 15:20:21 -0800

    Update MainDrive.java

--- Main Drivetrain Code/MainDrive.java
+++ Main Drivetrain Code/MainDrive.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
-@TeleOp(name = "MainDriveCopy 11 Nov. 2024")
-public class _11112024MainDriveCopy extends LinearOpMode {
+@TeleOp(name = "MainDrive")
+public class MainDrive extends LinearOpMode {
 
   private DcMotor frontright;
   private DcMotor rearright;
